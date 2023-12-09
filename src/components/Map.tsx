3ac08c12d6ad4d20635759066cf491118bbc4d90
Angela Olovson <angela.olovson@gmail.com>
2023-12-09 16:25:30 -0500

    Created Map componenet

---
+++ src/components/Map.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Map: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center bg-navFooter">
+            <div>
+                <a href="/">屏東縣新埤鄉打鐵村東興路164號</a>
+                
+            </div>
+            <div>
+                map
+            </div>
+            <div>
+                map
+            </div>
+        </div>
+    );
+}
+
+export default Map;
