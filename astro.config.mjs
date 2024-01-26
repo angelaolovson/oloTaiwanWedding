44b935148e02590eb95b514dd4594a942eff93e0
Angela Olovson <angela.olovson@gmail.com>
2024-01-26 17:44:15 -0500

    Updated base parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://angelaolovson.github.io',
+  base: '/oloTwWedding',
 });
